2aac1021d74a8aabe9e31f962d30181f0ba5088b
Eoin Francis <eoin.francis3@mail.dcu.ie>
2024-10-16 17:45:35 +0100

    User registration system configured. We will need to differentiate between students and lab tutors.

--- src/backend/queueing_system/urls.py
+++ src/backend/queueing_system/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name="index")
+   path('', views.index, name="index"),
+   path('sign_up/', views.UserSignupView.as_view(), name="sign_up"),
 ] 
--- src/backend/queueing_system/views.py
+++ src/backend/queueing_system/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import *
 
 def index(request):
     return render(request, 'index.html')
 
-# Create your views here.
+class UserSignupView(CreateView):
+    model = User
+    form_class = UserSignupForm
+    template_name = 'user_signup.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
